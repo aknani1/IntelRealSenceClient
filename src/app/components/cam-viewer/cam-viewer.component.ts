67ea74f43ba0b89fb17c89a7c8d0dca3bc123a86
aknani1 <raoof.knani@gmail.com>
2025-01-11 23:24:22 +0200

    new_version

--- src/app/components/cam-viewer/cam-viewer.component.ts
+++ src/app/components/cam-viewer/cam-viewer.component.ts
@@ -1,72 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { io } from 'socket.io-client';
 import { WebSocketService } from '../../services/web-socket.service';
-import { NgIf } from '@angular/common';
 import { HttpConfigService } from '../../services/http-config.service';
 
 @Component({
   selector: 'app-cam-viewer',
   standalone: false,
   templateUrl: './cam-viewer.component.html',
-  styleUrl: './cam-viewer.component.scss'
+  styleUrls: ['./cam-viewer.component.scss'],
 })
-export class CamViewerComponent implements OnInit{
-  colorImageUrl: string = '';
-  depthImageUrl: string = '';
-  showRGB: boolean = true;
-  showDepth: boolean = true;
-  depthResolution = '640x480';
-  depthFrameRate = '30';
-  rgbResolution = '640x480';
-  rgbFrameRate = '30';
+export class CamViewerComponent implements OnInit {
+   // Sidebar toggles
+   depthModuleEnabled: boolean = false;
+   rgbCameraEnabled: boolean = false;
+ 
+   // Control panel visibility
+   depthModuleVisible: boolean = false;
+   rgbCameraVisible: boolean = false;
+ 
+   // Stream settings
+   showDepth: boolean = false;
+   showRGB: boolean = false;
+   depthImageUrl: string = '';
+   colorImageUrl: string = '';
+ 
+   // Control panel settings
+   selectedDepthResolution: string = '640x480';
+   selectedDepthFrameRate: string = '30';
+   selectedRGBResolution: string = '640x480';
+   selectedRGBFrameRate: string = '30';
+ 
+  constructor(
+    private webSocketService: WebSocketService,
+    private httpConfigService: HttpConfigService
+  ) {}
 
-
-
-  stereoModuleEnabled = false;
-  rgbCameraEnabled = false;
-
-  // Control table settings
-  selectedResolution = '640x480';
-  selectedFrameRate = '30';
-  exposureValue = '8500';
-
-  // Stream state
-  streamActive = false;
-  // Controls data for the table
-  controls = [
-    {
-      name: 'Resolution',
-      options: ['640x480', '1280x720'],
-      currentValue: this.selectedResolution,
-    },
-    {
-      name: 'Frame Rate (FPS)',
-      options: ['15', '30'],
-      currentValue: this.selectedFrameRate,
-    },
-    {
-      name: 'Exposure',
-      options: null, // Exposure uses a slider, so no dropdown options are needed
-      currentValue: this.exposureValue,
-    },
-  ];
-
-  displayedColumns = ['name', 'currentValue', 'options'];
-
-
-  // Selected settings
-  selectedDepthResolution = this.depthResolution;
-  selectedDepthFrameRate = this.depthFrameRate;
-  selectedRGBResolution = this.rgbResolution;
-  selectedRGBFrameRate = this.rgbFrameRate;
-  constructor(private WebSocketService :WebSocketService,
-              private httpConfigService:HttpConfigService
-
-  ){}
-
-  ngOnInit() {
-    this.WebSocketService.startStream();
-    this.WebSocketService.getVideoStream().subscribe(frame => {
+  ngOnInit(): void {
+    this.webSocketService.startStream();
+    this.webSocketService.getVideoStream().subscribe((frame) => {
       if (this.showRGB) {
         this.colorImageUrl = 'data:image/jpeg;base64,' + frame.color;
       } else {
@@ -78,59 +48,43 @@ export class CamViewerComponent implements OnInit{
         this.depthImageUrl = '';
       }
     });
-    }
-    toggleRGB() {
-      this.showRGB = !this.showRGB; // Toggle RGB stream
-    }
-  
-    toggleDepth() {
-      this.showDepth = !this.showDepth; // Toggle Depth stream
-    }
- // Methods to update settings
- updateResolution(): void {
-  console.log('Resolution updated to:', this.selectedResolution);
-  this.controls[0].currentValue = this.selectedResolution;
-  // Add logic to send resolution update to server
-}
+  }
 
-updateFrameRate(): void {
-  console.log('Frame rate updated to:', this.selectedFrameRate);
-  this.controls[1].currentValue = this.selectedFrameRate;
-  // Add logic to send frame rate update to server
-}
+  // Toggle Depth Module
+  toggleDepthModule(): void {
+    this.depthModuleVisible = !this.depthModuleVisible;
+    this.showDepth = this.depthModuleEnabled;
+    console.log('Depth Module:', this.depthModuleEnabled ? 'Enabled' : 'Disabled');
+  }
 
-updateExposure(): void {
-  console.log('Exposure updated to:', this.exposureValue);
-  this.controls[2].currentValue = this.exposureValue;
-  // Add logic to send exposure update to server
-}
- updateDepthConfiguration(): void {
-  this.httpConfigService.updateConfiguration(
-    'depth',
-    this.selectedDepthResolution,
-    this.selectedDepthFrameRate
-  ).subscribe(
-    (response) => {
-      console.log('Depth Configuration Updated:', response);
-      this.depthResolution = response.resolution;
-      this.depthFrameRate = response.frame_rate;
-    },
-    (error) => console.error('Error updating depth configuration:', error)
-  );
-}
+  // Toggle RGB Camera
+  toggleRGBCamera(): void {
+    this.rgbCameraVisible = !this.rgbCameraVisible;
+    this.showRGB = this.rgbCameraEnabled;
+    console.log('RGB Camera:', this.rgbCameraEnabled ? 'Enabled' : 'Disabled');
+  }
 
-updateRGBConfiguration(): void {
-  this.httpConfigService.updateConfiguration(
-    'rgb',
-    this.selectedRGBResolution,
-    this.selectedRGBFrameRate
-  ).subscribe(
-    (response) => {
-      console.log('RGB Configuration Updated:', response);
-      this.rgbResolution = response.resolution;
-      this.rgbFrameRate = response.frame_rate;
-    },
-    (error) => console.error('Error updating RGB configuration:', error)
-  );
-}
+  // Update Depth Resolution
+  updateDepthResolution(): void {
+    console.log('Updated Depth Resolution:', this.selectedDepthResolution);
+    // Add logic to send resolution update to server
+  }
+
+  // Update Depth Frame Rate
+  updateDepthFrameRate(): void {
+    console.log('Updated Depth Frame Rate:', this.selectedDepthFrameRate);
+    // Add logic to send frame rate update to server
+  }
+
+  // Update RGB Resolution
+  updateRGBResolution(): void {
+    console.log('Updated RGB Resolution:', this.selectedRGBResolution);
+    // Add logic to send resolution update to server
+  }
+
+  // Update RGB Frame Rate
+  updateRGBFrameRate(): void {
+    console.log('Updated RGB Frame Rate:', this.selectedRGBFrameRate);
+    // Add logic to send frame rate update to server
+  }
 }
